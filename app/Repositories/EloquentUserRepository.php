b9a038e5511175df3ad483bfc48c5ffdb6df6b20
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-09 07:31:04 +0300

    Added eloquent user repository

---
+++ app/Repositories/EloquentUserRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use Core\User\User;
+use Core\User\UserFactory;
+use Core\User\UserRepository;
+use App\Models\User as DBUser;
+
+class EloquentUserRepository implements UserRepository
+{
+    private $userFactory;
+
+    public function __construct(UserFactory $userFactory)
+    {
+        $this->userFactory = $userFactory;
+    }
+
+    public function create(User $user): int
+    {
+        $createdUser = DBUser::query()->create([
+            'username' => $user->getUsername(),
+            'password' => $user->getPassword(),
+        ]);
+        $id = $createdUser->getId();
+        $user->setId($id);
+        return $id;
+    }
+
+    public function findByUsername(string $username): ?User
+    {
+        $founded = DBUser::query()->where('username', $username)->first();
+        if (!$founded) {
+            return null;
+        }
+        return $this->userFactory->create($founded->getUsername(), $founded->getPassword());
+    }
+
+    public function getByID(int $id): ?User
+    {
+        $founded = DBUser::query()->find($id);
+        if (!$founded) {
+            return null;
+        }
+        return $this->userFactory->create($founded->getUsername(), $founded->getPassword());
+    }
+}
