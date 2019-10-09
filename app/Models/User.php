ba0dcadc795166dacc26fe339a52d43689e716c5
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-09 07:30:37 +0300

    Added table and eloquent model for users

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['username', 'password'];
+    protected $guarded = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
