0ebc8bbb0f7631c96eab2c1d9a1cfd843d5ee7b4
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-12 16:41:57 +0300

    Added getters for task db model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,13 +3,58 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     public $timestamps = false;
+    protected $guarded = ['id'];
+    protected $casts = [
+        'deadline' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+
+    public function getDeadline(): \DateTime
+    {
+        return $this->deadline;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getProject(): Project
+    {
+        return $this->project;
     }
 }
