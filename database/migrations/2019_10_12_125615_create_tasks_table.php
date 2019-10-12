1fc7586a435225828f2c2754f4d2e4b06a1a533b
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-12 15:59:14 +0300

    Added migration and model for task

---
+++ database/migrations/2019_10_12_125615_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('status')->default('undone');
+            $table->unsignedInteger('priority');
+            $table->dateTime('deadline')->nullable();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->on('projects')->references('id')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
