2b4a57ab96dcd87119581256d152e0c29ad39ea2
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-12 22:15:16 +0300

    Added sass resources loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+require('laravel-mix-sass-resources-loader');
 
 mix.js('resources/js/app.js', 'public/js').
     sass('resources/sass/app.scss', 'public/css').
+    sassResources('./resources/sass/_variables.scss').
     webpackConfig({
         plugins: [
             new VuetifyLoaderPlugin(),
