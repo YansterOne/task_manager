835b2f26367f7a0873c21924b01b8a7e3c8c1d81
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-12 19:49:50 +0300

    Init frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css) }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
 <div id="app"></div>
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
