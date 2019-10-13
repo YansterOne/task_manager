307f94772a3eeb34cd05e4e470ae503d13bd2144
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-13 21:37:25 +0300

    Fixed checking undefined values

--- resources/js/mixins/validation.js
+++ resources/js/mixins/validation.js
@@ -3,7 +3,7 @@ export default {
         return {
             rules: {
                 required: value => !!value || 'Required.',
-                maxLength: value => value.length <= 255 || 'Max 255 characters',
+                maxLength: value => (value && value.length <= 255) || 'Max 255 characters',
                 email: value => {
                     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                     return pattern.test(value) || 'Invalid e-mail.';
