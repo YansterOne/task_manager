972a1a009332ca041bff1769cb6b6b48d8244ad6
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-10-14 08:52:21 +0300

    Removed transition for pages

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,8 @@
 <template>
     <v-app>
-        <transition-group name="fade" mode="out-in">
-            <app-header v-if="isAuthorized" key="header"></app-header>
-            <login-page v-if="!isAuthorized" key="login"></login-page>
-            <projects-page v-else key="projects"></projects-page>
-        </transition-group>
+        <app-header v-if="isAuthorized" key="header"></app-header>
+        <login-page v-if="!isAuthorized" key="login"></login-page>
+        <projects-page v-else key="projects"></projects-page>
         <notification-message></notification-message>
     </v-app>
 </template>
